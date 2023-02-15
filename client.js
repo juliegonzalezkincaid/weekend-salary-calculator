15533d2aee4d3a58a9d16400312db90765977938
julie gonzalez-kincaid <juliegonzalezkincaid@gmail.com>
2023-02-15 18:25:26 -0500

    started putting in the basics

--- client.js
+++ client.js
@@ -1,1 +1,39 @@
 console.log(Hello world!);
+
+// The application should have an input form that collects _
+// employee first name, last name, ID number, job title, annual salary_.
+
+function submitForm(event){
+    console.log('submitForm');
+    event.preventDefault();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
